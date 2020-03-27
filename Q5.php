afdf7f412327467c9d5ff630cbcf42608a4f66d5
megan-portelli <meganportelli15@gmail.com>
2020-03-27 13:52:27 +0100

    test

---
+++ Q5.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+
+<!--Code in form and PHP code relating to GET method is copied from Q4-->
+<!--Outputting Code is for Q5-->
+
+  <body>
+
+    <form method="GET">
+        Enter Name
+        <input type="text" name="person">
+        Enter Favourite Food
+        <input type="text" name="favFood">
+        Enter your ID Number
+        <input type="text" name="ID">
+        <input type="submit" name="submit">
+    </form>
+
+     <?php
+     //Copied from Q4
+     if (isset($_GET['submit'])) {
+         $name = $_GET['person'];
+         $food = $_GET['favFood'];
+         $UserID = $_GET['ID'];
+
+         echo "Successful Entry"; //Q5
+     } //till here
+
+     ?>
+
+
+  </body>
+
+</html>
