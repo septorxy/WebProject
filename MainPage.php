9f00a75f5a20e7106a1f50edcbef3594726d5671
megan-portelli <meganportelli15@gmail.com>
2020-04-06 13:26:29 +0200

    Update1

---
+++ MainPage.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Ta' Randi</title>
+    <link href=".../Uni_Project/STYLE.CSS" rel = stylesheet type = text/CSS>
+  </head>
+
+  <body>
+
+    <h1> Ta'Randi Restaurant </h1>
+
+    <div id = "left">
+    <ul>
+      <li> About us</li>
+      <li> Menu </li>
+      <li> Favourites </li>
+      <li> Contact Us </li>
+    </ul>
+  </div>
+
+  <div id = "bottom">
+    <a href = "https://www.facebook.com/Ta-Randi-508871642560125">Facebook</a>
+
+  </body>
+  
+</html>
