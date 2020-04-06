a47b3145e4eeb16d469a6e3047f5b6fb42aecd21
megan-portelli <meganportelli15@gmail.com>
2020-04-06 13:30:49 +0200

    3

--- MainPage.php
+++ MainPage.php
@@ -3,11 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title>Ta' Randi</title>
-<<<<<<< HEAD
     <link href="This PC/Documents/Uni_Project/STYLE.CSS" rel = stylesheet type = text/CSS>
-=======
-    <link href=".../Uni_Project/STYLE.CSS" rel = stylesheet type = text/CSS>
->>>>>>> 9f00a75f5a20e7106a1f50edcbef3594726d5671
   </head>
 
   <body>
@@ -27,9 +23,5 @@
     <a href = "https://www.facebook.com/Ta-Randi-508871642560125">Facebook</a>
 
   </body>
-<<<<<<< HEAD
 
-=======
-  
->>>>>>> 9f00a75f5a20e7106a1f50edcbef3594726d5671
 </html>
