f62a8df30688571c7564dbad3605e62aac8282e5
megan-portelli <meganportelli15@gmail.com>
2020-04-07 22:59:46 +0200

    Test

--- MainPage.php
+++ MainPage.php
@@ -1,27 +1,53 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
+<head>
     <meta charset="utf-8">
     <title>Ta' Randi</title>
-    <link href="C:\Users\Win10 User\Documents\GitHub\Uni_Project\STYLE.CSS" rel = stylesheet type = text/CSS>
-  </head>
+<link href="../css/Style.css" rel="stylesheet" type="text/css">
+</head>
 
-  <body>
 
-    <h1> Ta'Randi Restaurant </h1>
 
-    <div id = "left">
-    <ul>
-      <li> About us</li>
-      <li> Menu </li>
-      <li> Favourites </li>
-      <li> Contact Us </li>
-    </ul>
-  </div>
+<body>
 
-  <div id = "bottom">
-    <a href = "https://www.facebook.com/Ta-Randi-508871642560125">Facebook</a>
 
-  </body>
+
+<div id="top_left" >
+		<a href="Home Page.html">
+			<ul>
+			<li>Ta'Randi</li>
+			<li>Restaurant </li>
+			</ul>
+		</a>
+</div>
+
+
+
+<div id="top_right" >
+         <a href="../About Us/About Us.html" > About us </a>
+   		 <a href="Home Page.html"> Menu </a>
+         <a href="../Favourites/Favourites.html"> Favourites </a>
+   		 <a href="../Contact Us/Contact Us.html"> Contact Us </a>
+ </div>
+
+
+
+<div id="middle_sec">
+	HOME PAGE <br><br><br><br>
+
+	blablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablabla blablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablabla blablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablabla blablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablabla
+</div>
+
+
+
+<div id="bottom_sec">
+	<a href ="https://m.facebook.com/Ta-Randi-508871642560125/?_rdr" target="_blank">
+		<img src="../Images/logo-facebook-1.png" width="40" height="40" alt=""/>
+    </a>
+ </div>
+
+
+
+</body>
 
 </html>
