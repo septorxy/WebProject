00e8a8827a8d76e6b41ed36c727ef8c0e678f412
Nina <nina.musumeci.19@um.edu.mt>
2020-05-13 11:14:57 +0200

    CSS Changes

--- TaRandi wo vendor/bootstrap.php
+++ TaRandi wo vendor/bootstrap.php
@@ -2,9 +2,18 @@
     //Load our autoloader
     require_once __DIR__.'/vendor/autoload.php';
 
+    //FOR TWIG 2.0
     //Specify our Twig templates location
     $loader = new \Twig\Loader\FilesystemLoader('templates');
 
 
     //Instantiates our Twig
     $twig = new \Twig\Environment($loader);
+
+    /* FOR OTHER TWIG VERSIONS
+    //Specify our Twig templates location
+    $loader = new \Twig\Loader\FilesystemLoader('templates');
+
+
+    //Instantiates our Twig
+    $twig = new \Twig\Environment($loader);*/
